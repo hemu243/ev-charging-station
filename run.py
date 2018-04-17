25f34a4e8fc886c9504235af1a69cbfcb57acf73
Hemendra Choudhary <hchoudhary@splunk.com>
2018-04-16 17:11:40 -0700

    Format code

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
+import argparse
+
 from data_initializer import stations, SingletonClass
 from graph_lib import path
-import argparse
+
 
 class Runner(object):
 	"""
 	Main class which is used to run this project
 	"""
+
 	def __init__(self, distance_threshold, api_key):
 		"""
 		:param distance_threshold: distance threshold which is being used by algorithm to calculate
@@ -20,8 +23,8 @@ class Runner(object):
 
 	def run(self):
 		"""
-			Add run logic here
-		:return:
+		Running algorithm here
+		:return: None
 		"""
 		singleton_stations = SingletonClass(stations.Stations)
 		existing_stations = singleton_stations()
@@ -29,14 +32,17 @@ class Runner(object):
 		data = existing_stations.simplified_data()
 		# TODO - change me (Sample code to calculate path)
 		# Calculate path between two points
-		print path.Path.get_direction(data[0]["Full Address"], data[1]["Full Address"], self.api_key)
+		print path.Path.get_direction(data[0]["Full Address"], data[1]["Full Address"],
+									  self.api_key)
 
 
 def parse_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-t', '--threshold', help="threshold distance between points", required=True, type=float)
+	parser.add_argument('-t', '--threshold', help="threshold distance between points",
+						required=True, type=float)
 	parser.add_argument('-a', '--api_key', help="Google api key for distance matrix,"
-												" refer - https://developers.google.com/maps/documentation/distance-matrix/get-api-key for more infromation", required=True, type=str)
+												" refer - https://developers.google.com/maps/documentation/distance-matrix/get-api-key for more infromation",
+						required=True, type=str)
 	args = parser.parse_args()
 	return args
 
